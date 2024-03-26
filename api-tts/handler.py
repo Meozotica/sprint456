3883742010c49136c329ad85219f1dc8b8ab5236
meozotica <meozoticaaminosse5@gmail.com>
2024-03-27 00:06:23 +0200

    instalacao e importacao  do boto3,criacao do s3

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -1,5 +1,23 @@
 import json
+import boto3
+ 
 
+bucket_name = "armazena-frases"
+s3 = boto3.resource("s3") 
+bucket_existe = False
+
+# Print out bucket names
+for bucket in s3.buckets.all():
+    if bucket.name== bucket_name:
+        bucket_existe = not bucket_existe  
+
+    print(bucket.name)
+
+if bucket_existe == False:
+    s3.create_bucket(bucket_name)
+
+else:
+    print("error: O bucket ja existe")
 
 def hello(event, context):
     body = {
@@ -29,6 +47,14 @@ def v1Description(event, context):
 def v2Description(event, context):
     body = {
         "message": "TTS api version 2.!",
+        # "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+def parte1(event, context):
+    body = {
+        "phrase": "converta esse texto para áudio!",
         # "input": event,
     }
 
