c0e6623b702d2174414571a3d2f1552878b8bf53
meozotica <meozoticaaminosse5@gmail.com>
2024-03-26 22:37:06 +0200

    Criacao das 3 primeiras rotas

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -8,3 +8,28 @@ def hello(event, context):
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def health(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        # "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+def v1Description(event, context):
+    body = {
+        "message": "TTS api version 1.!",
+        # "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+def v2Description(event, context):
+    body = {
+        "message": "TTS api version 2.!",
+        # "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
