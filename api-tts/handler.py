633a49c754ff70f31e29c603a2e88a1126a760f3
Wady Paulino <wadymunguambe73@gmail.com>
2024-03-28 22:41:34 +0200

    cria bucket e adiciona permissao de accesso ao servico s3 para as lamda functions

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -1,23 +1,40 @@
 import json
 import boto3
- 
 
-bucket_name = "armazena-frases"
-s3 = boto3.resource("s3") 
-bucket_existe = False
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    if bucket.name== bucket_name:
-        bucket_existe = not bucket_existe  
+# ================================ DEVELOPER FUNCIONS ================================
+
+def createBucketMethod():
+    bucket_name = "armazena-frases"
+    s3 = boto3.resource("s3") 
+    bucket_existe = False
+
+    # Print out bucket names
+    for bucket in s3.buckets.all():
+        if bucket.name == bucket_name:
+            bucket_existe = not bucket_existe  
 
     print(bucket.name)
 
-if bucket_existe == False:
-    s3.create_bucket(bucket_name)
+    if bucket_existe == False:
+        s3.create_bucket(Bucket=bucket_name)
+        body = {
+            "message": "Bucket created successfully!! Your function executed successfully!",
+            "bucketName" : f"{bucket_name}"
+        }
+        
+        return {"statusCode" : 200, "body" : json.dumps(body)}
+    else:
+        body = {
+            "message": "The bucket was already created!",
+            "bucketName" : f"{bucket_name}"
+        }
+        
+        return {"statusCode" : 200, "body" : json.dumps(body)}
+    
+    
 
-else:
-    print("error: O bucket ja existe")
+# ================================ SERVERLESS FUNCIONS ================================
 
 def hello(event, context):
     body = {
@@ -27,14 +44,13 @@ def hello(event, context):
 
     return {"statusCode": 200, "body": json.dumps(body)}
 
-
-def health(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        # "input": event,
-    }
-
-    return {"statusCode": 200, "body": json.dumps(body)}
+def health(event, context):    
+    # body = {
+    #     "message": "Go Serverless v3.0! Your function executed successfully!",
+    #     # "input": event,
+    # }
+    
+    return createBucketMethod()
 
 def v1Description(event, context):
     body = {
